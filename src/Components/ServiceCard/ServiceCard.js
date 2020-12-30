9cde47e49910ccdb84b67c83371bf26d714a9c3b
Ida Bagus Wedagama <wardana.wedagama@gmail.com>
2020-12-30 21:40:27 +0800

    [Weda] Fix service section

--- src/Components/ServiceCard/ServiceCard.js
+++ src/Components/ServiceCard/ServiceCard.js
@@ -4,38 +4,42 @@ import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward'
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward'
 
 function ServiceCard(props) {
-  let CardElement = (
-    <div className={classes.CardElement}>
-      <div className={classes.ShowButton} onClick={props.toggleService}>
-        <ArrowUpwardIcon />
-      </div>
-      <div className={classes.Info}>
-        <h3 className={classes.Title}>{props.title}</h3>
-        <p className={classes.Subtitle}>{props.subtitle}</p>
-      </div>
-    </div>
-  )
+	let CardElement = (
+		<div className={classes.CardElement}>
+			<div className={classes.Info}>
+				<h3 className={classes.Title}>{props.title}</h3>
+				<p className={classes.Subtitle}>{props.subtitle}</p>
+			</div>
+			<div className={classes.ExpandButton}>
+				<button onClick={props.toggleService}>
+					<p className={classes.Button}>Show More</p>
+				</button>
+			</div>
+		</div>
+	)
 
-  if (props.showService) {
-    CardElement = (
-      <div className={classes.CardElementExpand}>
-        <div className={classes.ShowButton} onClick={props.toggleService}>
-          <ArrowDownwardIcon />
-        </div>
-        <div className={classes.InfoExpand}>
-          <h3 className={classes.Title}>{props.title}</h3>
-          <p className={classes.Subtitle}>{props.fulldesc}</p>
-        </div>
-      </div>
-    )
-  }
+	if (props.showService) {
+		CardElement = (
+			<div className={classes.CardElement}>
+				<div className={classes.InfoExpand}>
+					<h3 className={classes.Title}>{props.title}</h3>
+					<p className={classes.Subtitle}>{props.fulldesc}</p>
+				</div>
+				<div className={classes.ExpandButton}>
+					<button onClick={props.toggleService}>
+						<p className={classes.Button}>Show Less</p>
+					</button>
+				</div>
+			</div>
+		)
+	}
 
-  return (
-    <section className={classes.ServiceCard}>
-      <img src={props.image} alt={props.label} />
-      {CardElement}
-    </section>
-  )
+	return (
+		<section className={classes.ServiceCard}>
+			<img src={props.image} alt={props.label} />
+			{CardElement}
+		</section>
+	)
 }
 
 export default ServiceCard
